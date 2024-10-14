095b8a017eae29e1717eacaeea4afedcc1e58b9d
wldmsdl7 <wldmsdl5835@gmail.com>
2024-10-14 17:11:56 +0900

    Create main.dart

---
+++ 1014/1014 임지은/main.dart
@@ -0,0 +1,36 @@
+import 'package:u_and_i/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          fontFamily: 'sunflower', // 기본 글씨체
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(
+              color: Colors.white, //  글 색상
+              fontSize: 80.0, //  글 크기
+              fontWeight: FontWeight.w700, //  글 두께
+              fontFamily: 'parisienne', //  글씨체
+            ),
+            displayMedium: TextStyle(
+              color: Colors.white,
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700,
+            ),
+            bodyLarge: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
+            ),
+            bodyMedium: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+          )),
+      home: HomeScreen(),
+    );
+  }
+}
